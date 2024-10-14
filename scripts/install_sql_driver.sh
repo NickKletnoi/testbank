9a4407a54b947910e17261221d6b930785d28b15
Nick Kletnoi <nick.kletnoi@gmail.com>
2024-10-14 11:31:30 -0400

    Update install_sql_driver.sh

--- scripts/install_sql_driver.sh
+++ scripts/install_sql_driver.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-#
-
-if ! [[ "18.04 20.04 22.04 22.10" == *"$(lsb_release -rs)"* ]];
-then
-    echo "Ubuntu $(lsb_release -rs) is not currently supported.";
-    exit;
+# Check if the current Ubuntu version is supported
+if ! [[ "18.04 20.04 22.04 22.10" == *"$(lsb_release -rs)"* ]]; then
+    echo "Ubuntu $(lsb_release -rs) is not currently supported."
+    exit 1
 fi
 
-sudo su
-curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+# Add Microsoft package signing key
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 
-curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list > /etc/apt/sources.list.d/mssql-release.list
+# Add the Microsoft SQL Server repository
+curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list | sudo tee /etc/apt/sources.list.d/mssql-release.list
 
-exit
+# Update package lists
 sudo apt-get update
+
+# Install the ODBC Driver for SQL Server
 sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
-# optional: for bcp and sqlcmd
-#sudo ACCEPT_EULA=Y apt-get install -y mssql-tools18
-#echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bashrc
-#source ~/.bashrc
-# optional: for unixODBC development headers
-#sudo apt-get install -y unixodbc-dev
+
+# Optional: Install tools for BCP and SQLCMD
+# sudo ACCEPT_EULA=Y apt-get install -y mssql-tools18
+# echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bashrc
+# source ~/.bashrc
+
+# Optional: Install unixODBC development headers
+# sudo apt-get install -y unixodbc-dev
