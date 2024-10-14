d9eb015000036949ebd4ab8525f4ba04928d0c67
Nick Kletnoi <nick.kletnoi@gmail.com>
2024-10-14 13:46:58 -0400

    Update install_sql_driver.sh

--- scripts/install_sql_driver.sh
+++ scripts/install_sql_driver.sh
@@ -2,20 +2,3 @@
 
 sudo su
 sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
-
-
-
-# if ! [[ "18.04 20.04 22.04 22.10 24.04" == *"$(lsb_release -rs)"* ]]; then
-#     echo "Ubuntu $(lsb_release -rs) is not currently supported."
-#     exit
-# fi
-# #curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list > /etc/apt/sources.list.d/mssql-release.list
-# curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-# sudo apt-get update
-# sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
-# optional: for bcp and sqlcmd
-# sudo ACCEPT_EULA=Y apt-get install -y mssql-tools18
-# echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bashrc
-# source ~/.bashrc
-# optional: for unixODBC development headers
-# sudo apt-get install -y unixodbc-dev
